7cd6c1851f24b26e5cab06e4bbd0b874570b70aa
JoséMiguel Marte <tumusicalatina@gmail.com>
2024-05-15 20:12:00 -0400

    new class

---
+++ 202404/myapps/202405/destructuring/index.js
@@ -0,0 +1,10 @@
+const employee = {
+    fname: "Billy",
+    lname: "Gates",
+    dept:'Executives',
+    salary: 1000000,
+};
+
+const { FirstName, LastName, departamento, money, hiredate } = employee;
+
+console.log(employee);
