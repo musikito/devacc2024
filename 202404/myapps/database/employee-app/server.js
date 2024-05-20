b9cb839b74f38fbb520e62ec11f332bc12ee4823
JoséMiguel Marte <tumusicalatina@gmail.com>
2024-05-20 17:00:37 -0400

    added CRUD

--- 202404/myapps/database/employee-app/server.js
+++ 202404/myapps/database/employee-app/server.js
@@ -1,32 +1,46 @@
+require('dotenv').config();
 const express = require('express');
-const mysql = require('mysql2/promise'); // For MySQL
-const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
 const path = require('path');
-
 const app = express();
-const port = 3000;
 
-// Database connection details (replace with your actual credentials)
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Papiton#1',
-  database: 'humanresources',
-});
+app.set('view engine', 'ejs');
 
-app.set('views', path.join(__dirname,'views')); // Set the views directory
-app.set('view engine', 'ejs'); // Set EJS as the view engine
+// Create a connection to the database
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME, 
+    port: process.env.DB_PORT,
+  });
 
-app.get('/', async (req, res) => {
-  try {
-    const [rows] = await pool.query('SELECT * FROM employees');
-    res.render('index', { employees: rows }); // Pass data to the template
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error fetching employee data');
-  }
-});
+  // Connect to the database
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to the database:', err.stack);
+      return;
+    }
+    console.log('Connected to the database.');
+  });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  // Middleware to parse request
+  app.use(bodyParser.urlencoded({ extended: true }));
+  // Serve static files
+  app.use(express.static(path.join(__dirname, 'public')));
+
+// Display all employees
+app.get('/', (req, res) => {
+    connection.query('SELECT * FROM employees', (err, rows) => {
+        if (err) throw err;
+        res.render('index', { employees: rows });
+    });
+}); 
+  
+
+
+  // run the server
+  app.listen(3000, () => {
+    console.log(`Server is running`);
 });
